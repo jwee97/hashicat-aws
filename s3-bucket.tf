a30f79ccea8b6430b61a4e87d511c0a053474451
Joshua Wee <joshuaftw000@gmail.com>
2023-02-17 06:55:52 +0000

    Change module 2

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,8 +1,7 @@
 module "s3_bucket" {
   source  = "app.terraform.io/test-1234567890/s3-bucket/aws"
   version = "2.8.0"
-
-  bucket = "my-s3-bucket"
+  
   acl    = "private"
   bucket_prefix = "joshua"
 
